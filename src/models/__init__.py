fa81d08dd1efe5e6dab3d0870b3f1ae900322909
ebao <ebao@toolsgroup.com>
2019-12-30 10:07:16 +0100

    [src.models] removed invalid model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,7 @@
 from src.models.atlstm import ATLSTM
-from src.models.atlstm_loss_sum import ATLSTM_loss_sum
 from src.models.atlx import ATLX
 
 VALID_MODELS = dict(
     atlstm=ATLSTM,
-    atlstm_loss_sum=ATLSTM_loss_sum,
     atlx=ATLX
 )
